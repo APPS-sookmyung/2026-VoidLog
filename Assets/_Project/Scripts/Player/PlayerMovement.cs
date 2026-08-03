6ed70939354a602f4457352d97059fbded7f2d4b
jihyun41 <jihyun418@sookmyung.ac.kr>
2026-08-04 01:54:20 +0900

    feat: 편지 화면 캔버스 추가
    - 편지 화면 띄우는 캔버스 추가
    - 플레이어 이동 스크립트 주석 추가
    - 주인공 오브젝트 변경 및 회전하는 버그 해결
    - 씬 이름 변경

--- Assets/_Project/Scripts/Player/PlayerMovement.cs
+++ Assets/_Project/Scripts/Player/PlayerMovement.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 //CanMove()를 통해 주인공 동작 불가능/가능 제어
 public class PlayerMovement : MonoBehaviour
 {
-    public bool CanMove = true;
-    [SerializeField] private float moveSpeed = 3f;
+    public bool CanMove = true; // 이동 기본 설정 
+    [SerializeField] private float moveSpeed = 3f; // 기본 스피드 값
 
     private Rigidbody2D rb;
     private Vector2 moveInput;
 
-    public bool getCanMove(){return CanMove;}
-    public void setCanMove(bool state) {CanMove = state;}
+    public bool getCanMove(){return CanMove;} // 현재 이동 가능 여부 확인
+    public void setCanMove(bool state) {CanMove = state;} // 이동 가능 여부 변경
 
 
     private void Awake() {
@@ -20,16 +20,17 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        if (!CanMove)
+        if (!CanMove) // 이동 불가 상태면 백터 값을 0으로 설정
         {
             rb.linearVelocity = Vector2.zero;
             return;
         }
  
-        float h = Input.GetAxisRaw("Horizontal");
-        float v = Input.GetAxisRaw("Vertical");
-        Vector2 dir = new Vector2(h, v).normalized;
+        float h = Input.GetAxisRaw("Horizontal"); // x값확인 - A,D 움직일 때
+        float v = Input.GetAxisRaw("Vertical"); // Y값 확인 - W,S 움직일 때
+        Vector2 dir = new Vector2(h, v).normalized; 
+        // 움직인 값 확인해서 새롭게 값 초기화 + 대각선으로 갈 때 속도 일정하게 맞추기 위해 normalized
  
-        rb.linearVelocity = dir * moveSpeed;
+        rb.linearVelocity = dir * moveSpeed; 
     }
 }
