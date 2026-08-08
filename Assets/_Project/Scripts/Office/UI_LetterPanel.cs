60e35f8f738ff69ac812ff9341b71f974d3fce02
jihyun41 <jihyun418@sookmyung.ac.kr>
2026-08-08 14:12:10 +0900

    Reapply "feat: 작업실: 금기사항 파일 오픈"
    This reverts commit 1dd150be579226518e170aaa646ef69d57c02fd5.

---
+++ Assets/_Project/Scripts/Office/UI_LetterPanel.cs
@@ -0,0 +1,24 @@
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+//편지 캔버스에 넣는 스크립트
+// 플레이어가 접근해서 E 클릭시 편지 오브젝트 화면에 띄움
+public class UI_LetterPanel : MonoBehaviour
+{
+    [SerializeField] private LetterSO letter; // 편지 데이터 가져오기
+
+    [SerializeField] private Canvas letterCanvas; // 편지 캔버스
+    [SerializeField] private TextMeshProUGUI content; // 편지 캔버스 속 텍스트 
+
+
+    private void Start() {
+        Display();
+    }
+    void Display() 
+    {
+        // 편지내용 가져오기
+        content.text=letter.getContent();
+    }
+
+}
--- Assets/_Project/Scripts/Puzzle/InteractableObject.cs
+++ Assets/_Project/Scripts/Puzzle/InteractableObject.cs
@@ -6,16 +6,40 @@ using UnityEngine.UI;
 
 // 플레이어와 상호작용하는 오브젝트(편지, 키패드, 모니터화면 등등)에 넣는 스크립트
 //콜라이더 구역 안에 플레이어 접근 시 [E] 클릭 텍스트 표시
+
 public class InteractableObject : MonoBehaviour
 {
+
+    [Tooltip("화면 하단에 띄울 문구 (ex. [E] 클릭)")]
     [SerializeField] TextMeshProUGUI clickText; // 표시할 텍스트
 
+    [Tooltip("[E] 클릭 후 띄우는 Cavas")]
+    [SerializeField] Canvas canvas; // E 클릭 시 띄울 캔버스
+
     private bool isClick; // 클릭 가능 여부
+ 
 
     void Start()
     {
-        // 시작할 때는 글자 가림
+        // E 클릭 멘트 및 캔버스 가림
         clickText.gameObject.SetActive(false); 
+        canvas.gameObject.SetActive(false);
+    }
+    
+    void Update()
+    {
+        // E클릭 가능한 범위면서 E클릭시 캔버스 표시
+        if (isClick)
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                canvas.gameObject.SetActive(true);  
+            }
+        }
+        else // 범위 나가면 캔버스 가리기
+        {
+            canvas.gameObject.SetActive(false);
+        }
     }
  
     void OnTriggerEnter2D(Collider2D collision)
@@ -37,8 +61,5 @@ public class InteractableObject : MonoBehaviour
         }
     }
 
-    public bool getIsClick() // 클릭 가능 여부 반환
-    {
-        return isClick;
-    }
+
 }
